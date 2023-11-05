588f6f9cdf3e2299750d38ec1fcf755f53afb096
mr-temitayo <teeakintoye@yahoo.com>
2023-11-05 16:18:39 +0100

    szymon

---
+++ students/moseswong/notes/ifelse.py
@@ -0,0 +1,34 @@
+#the if else statement is a condition 
+# which allows python to make a decision if the condition is True or False
+
+# example 1
+# if rain is falling (condition True)
+# get an umbrella (decision)
+
+# else (if rain is not falling)
+# go without the umbrella
+
+
+# example 2
+# if student is in class (condition True)
+#     start teaching
+
+# else (no student in class)
+# do not teach yet
+
+# example 3
+# if you are hungry (condition True)
+# buy food
+
+# else (not hungry)
+# dont buy food yet
+
+
+# Python example
+# Moses-
+# Ask for the name of a student
+# Ask for his score in maths, english, science, and computer  
+# get the total score
+# Mr. Tee-
+# If the student total score is above 300 tell him he passed the exams
+# else tell he sorry, he failed
