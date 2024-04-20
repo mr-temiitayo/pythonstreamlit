332b19c26f7d20a09424e3409537457f4c83e7bf
mr-temitayo <teeakintoye@yahoo.com>
2024-04-20 16:26:39 +0100

    donor

--- students/FaisalnAisha/donor.py
+++ students/FaisalnAisha/donor.py
@@ -5,7 +5,7 @@ donordata=pd.read_csv('blood_donor.csv')
 menu=st.sidebar.selectbox("Menu",['Registration','Database'])
 if menu == 'Registration':
     st.title(':blue[Register For Blood Donation]')
-    col1,col2,col3=st.columns(3)
+    col1,col2,col3=st.columns([1.5,1,1])
     with col1:
         st.header("Personal Information")
         name=st.text_input('Input Your Full Name')
@@ -19,15 +19,16 @@ if menu == 'Registration':
         ill=st.radio('Do You Have Any Medical Conditions?',check,horizontal = True)
         insured=st.radio('Do You Have Health Insurance?',check,horizontal = True)
         firstime=st.radio('Have You Donated Blood Before?',check,horizontal= True)
-    colu1,colu2,colu3=st.columns([2,0.5,2])
-    with col2:
-        with colu2:
-            if st.button("Save Donor Information"):
-                st.success("Data Saved")
-                donordict={'Full Name':[name],'Phone Number':[num],'Donor Age':[age],'Blood Type':[blood],'Medical Condition':[ill],'Insurance Coverage':[insured],"Gender":[gen], 'Prior Donations':[firstime]}
-                donorDF=pd.DataFrame(donordict)
-                combinedata=pd.concat([donorDF,donordata],ignore_index= True)
-                combinedata.to_csv('blood_donor.csv',index=False)
+    colu1,colu2,colu3=st.columns([1.5,1,1])
+    with colu2:
+            col2a,col2b,col2c = st.columns([1,2,1])
+            with col2b:
+                if st.button("Save Donor Info"):
+                    st.success("Donor Data Saved")
+                    donordict={'Full Name':[name],'Phone Number':[num],'Donor Age':[age],'Blood Type':[blood],'Medical Condition':[ill],'Insurance Coverage':[insured],"Gender":[gen], 'Prior Donations':[firstime]}
+                    donorDF=pd.DataFrame(donordict)
+                    combinedata=pd.concat([donordata,donorDF],ignore_index= True)
+                    combinedata.to_csv('blood_donor.csv',index=False)
 if menu =='Database':
     st.header(":orange[DATABASE]")
     st.table(donordata)
